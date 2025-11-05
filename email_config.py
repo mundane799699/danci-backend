cef71c8471639c1752f9974773b709a265333b79
mundane <799699348@qq.com>
2025-11-05 09:09:24 +0800

    添加图片

--- email_config.py
+++ email_config.py
@@ -4,4 +4,7 @@
 QUOTE_IMAGES = [
     "https://cdn.mundane.ink/20251104103135213.png",
     "https://cdn.mundane.ink/20251104104320940.png",
+    "https://cdn.mundane.ink/20240525_094227.jpg",
+    "https://cdn.mundane.ink/20240906_230540.jpg",
+    "https://cdn.mundane.ink/20240902_091512.jpg",
 ]
