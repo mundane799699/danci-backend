912c6e6ae0a2453c090078ace8ac2b6ee4103dd0
mundane <799699348@qq.com>
2025-11-05 21:01:59 +0800

    修改配图配置

--- email_config.py
+++ email_config.py
@@ -3,8 +3,15 @@
 # 金句图片地址数组
 QUOTE_IMAGES = [
     "https://cdn.mundane.ink/20251104103135213.png",
-    "https://cdn.mundane.ink/20251104104320940.png",
+    "https://cdn.mundane.ink/202511052028926.jpg",
     "https://cdn.mundane.ink/20240525_094227.jpg",
     "https://cdn.mundane.ink/20240906_230540.jpg",
     "https://cdn.mundane.ink/20240902_091512.jpg",
+    "https://cdn.mundane.ink/202511052029261.jpg",
+    "https://cdn.mundane.ink/202511052030544.jpg",
+    "https://cdn.mundane.ink/202511052032576.jpg",
+    "https://cdn.mundane.ink/202511052033539.jpg",
+    "https://cdn.mundane.ink/202511052033139.jpg",
+    "https://cdn.mundane.ink/202511052034804.jpg",
+    "https://cdn.mundane.ink/202511052037807.jpg",
 ]
